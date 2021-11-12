5ee7ef4bdab595fe94a0204e78a6affe0600b19d
Mateusz Jurczak <jurczak.mateusz@gmail.com>
2021-11-12 12:28:47 +0100

    added configuration.h

---
+++ Configuration.h
@@ -0,0 +1,15 @@
+/*
+ * Configuration.h
+ *
+ *  Created on: Nov 12, 2021
+ *      Author: Mateusz
+ */
+
+#ifndef CONFIGURATION_H_
+#define CONFIGURATION_H_
+
+#define EMERGENCY_STOP_AXIS 4
+#define EMERGENCY_STOP_TIME_TRESHOLD_TICK 30
+#define EMERGENCY_STOP_AT_CONNECTION_LOSS
+
+#endif /* CONFIGURATION_H_ */
