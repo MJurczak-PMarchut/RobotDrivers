bd6b0decfa650b61f1b39bfb578addc0284e4f3d
Mateusz Jurczak <jurczak.mateusz@gmail.com>
2021-11-24 00:44:07 +0100

    Made Motor Control purely virtual

--- L9960T/L9960T.hpp
+++ L9960T/L9960T.hpp
@@ -11,9 +11,9 @@
 
 typedef enum {CURRENT_RANGE_0 = 0, CURRENT_RANGE_1 = 1, CURRENT_RANGE_2 = 2, CURRENT_RANGE_3 = 3} L9960T_CurrentRange;
 
-class L9960T : public MCInterface{
+class L9960T : protected MCInterface{
 	public:
-		L9960T(MotorSideTypeDef side, SPI_HandleTypeDef *hspi, uint16_t CS_Pin, GPIO_TypeDef);
+		L9960T(MotorSideTypeDef side, SPI_HandleTypeDef *hspi,  uint16_t CS_Pin, GPIO_TypeDef CS_Port, CommManager *CommunicationManager);
 		HAL_StatusTypeDef AttachPWMTimerAndChannel(TIM_HandleTypeDef *htim, uint32_t Channel);
 		HAL_StatusTypeDef SetMotorPowerPWM(uint16_t PowerPWM);
 		HAL_StatusTypeDef SetMotorDirection(MotorDirectionTypeDef Dir);
@@ -21,6 +21,17 @@ class L9960T : public MCInterface{
 		HAL_StatusTypeDef Disable();
 		HAL_StatusTypeDef CheckIfControllerInitializedOk(void);
 	private:
+		MotorSideTypeDef __side;
+		SPI_HandleTypeDef *__hspi;
+		uint16_t __CS_Pin;
+		uint16_t __IN1_PWM_PIN;
+		uint16_t __IN2_DIR_PIN;
+		GPIO_TypeDef __CS_Port;
+		GPIO_TypeDef *__IN1_PWM_PORT;
+		GPIO_TypeDef *__IN2_DIR_PORT;
+		CommManager *__CommunicationManager;
+		TIM_HandleTypeDef *__htim;
+		uint32_t __Channel;
 
 };
 
