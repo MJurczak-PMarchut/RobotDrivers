4211ed312d7532948fd1b9063ac05d61777849c6
Mateusz Jurczak <jurczak.mateusz@gmail.com>
2021-11-23 22:13:37 +0100

    add attach to header

--- L9960T/L9960T.hpp
+++ L9960T/L9960T.hpp
@@ -12,6 +12,7 @@
 class L9960T : public MCInterface{
 	public:
 		L9960T(MotorSideTypeDef side, SPI_HandleTypeDef *hspi, uint16_t CS_Pin, GPIO_TypeDef);
+		HAL_StatusTypeDef AttachPWMTimerAndChannel(TIM_HandleTypeDef *htim, uint32_t Channel);
 		HAL_StatusTypeDef SetMotorPowerPWM(uint16_t PowerPWM);
 		HAL_StatusTypeDef SetMotorDirection(MotorDirectionTypeDef Dir);
 		HAL_StatusTypeDef SetMaxCurrent(uint32_t MaxCurrent_mA);
