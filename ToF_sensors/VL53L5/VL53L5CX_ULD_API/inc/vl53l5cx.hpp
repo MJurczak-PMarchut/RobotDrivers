e7315263ad3560745ea113af9a268308a0f5336e
MJurczak <38139808+MJurczak-PMarchut@users.noreply.github.com>
2022-10-30 16:48:52 +0100

    init sensors

--- ToF_sensors/VL53L5/VL53L5CX_ULD_API/inc/vl53l5cx.hpp
+++ ToF_sensors/VL53L5/VL53L5CX_ULD_API/inc/vl53l5cx.hpp
@@ -13,15 +13,37 @@
 #include "ToFSensor.hpp"
 #include "vl53l5cx_api.h"
 
+#define DEFAULT_ADDR 0x52
+#define CHANGED_ADDRESS_OFFSET 0x10
+
 class Sensor_vl53l5cx: public ToF_Sensor
 {
 private:
 	static uint8_t __sensor_nb;
+	static uint8_t __sensor_init_tbd;
+	uint8_t __sensor_index;
 	VL53L5CX_Configuration __sensor_conf;
 public:
 	Sensor_vl53l5cx(e_ToF_Position position, CommManager *comm);
 	virtual HAL_StatusTypeDef SensorInit(void);
-	HAL_StatusTypeDef SetI2CAddress(uint8_t new_addr);
+	HAL_StatusTypeDef SetI2CAddress();
+	HAL_StatusTypeDef IsAlive(void);
+	HAL_StatusTypeDef GetPowerMode(void);
+	HAL_StatusTypeDef SetPowerMode(void);
+	HAL_StatusTypeDef StartRanging(void);
+	HAL_StatusTypeDef StopRanging(void);
+	HAL_StatusTypeDef CheckDataReady(void);
+	HAL_StatusTypeDef GetRangingData(void);
+	HAL_StatusTypeDef GetResolution(void);
+	HAL_StatusTypeDef SetResolution(void);
+	HAL_StatusTypeDef GetRangingFrequency(void);
+	HAL_StatusTypeDef SetRangingFrequency(void);
+	HAL_StatusTypeDef GetIntegrationTimeMs(void);
+	HAL_StatusTypeDef SetIntegrationTimeMs(void);
+	HAL_StatusTypeDef GetSharpenerPercent(void);
+	HAL_StatusTypeDef SetSharpenerPercent(void);
+	HAL_StatusTypeDef GetTargetOrder(void);
+	HAL_StatusTypeDef SetTargetOrder(void);
 	~Sensor_vl53l5cx(void) {};
 };
 
--- ToF_sensors/VL53L5/VL53L5CX_ULD_API/src/vl53l5cx.cpp
+++ ToF_sensors/VL53L5/VL53L5CX_ULD_API/src/vl53l5cx.cpp
@@ -7,17 +7,56 @@
 
 #include "vl53l5cx.hpp"
 
+const static uint8_t __ToFAddr [] = {0x54, 0x56, 0x58, 0x60, 0x62, 0x64};
+const static uint16_t __ToFX_SHUT_Pin [] = {XSHUT_3_Pin, XSHUT_3_Pin, XSHUT_3_Pin,
+											XSHUT_4_Pin, XSHUT_5_Pin, XSHUT_6_Pin};
+const static GPIO_TypeDef* __ToFX_SHUT_Port [] = {XSHUT_3_GPIO_Port, XSHUT_3_GPIO_Port,
+												 XSHUT_3_GPIO_Port, XSHUT_4_GPIO_Port,
+												 XSHUT_5_GPIO_Port, XSHUT_6_GPIO_Port};
+
 
 Sensor_vl53l5cx::Sensor_vl53l5cx(e_ToF_Position position, CommManager *comm):
 	ToF_Sensor(vl53l5, position, comm)
 {
-	__sensor_conf.platform.__CommunicationManager = comm;
+	this->__sensor_index = __sensor_nb;
+	this->__sensor_nb++;
+	this->__sensor_conf.platform.__CommunicationManager = comm;
+
+	HAL_GPIO_WritePin(__ToFX_SHUT_Port[this->__sensor_index], __ToFX_SHUT_Pin[this->__sensor_index], GPIO_PIN_RESET);
 }
 
+/*
+ * Initializes Sensors
+ * Could probably be made static to initialize all sensors present
+ * Or be implemented in parent class
+ */
 HAL_StatusTypeDef Sensor_vl53l5cx::SensorInit(void)
 {
 	HAL_StatusTypeDef ret = HAL_OK;
-	ret = HAL_StatusTypeDef(vl53l5cx_init(&(this->__sensor_conf)));
+	while(__sensor_init_tbd != this->__sensor_index)
+	{
+#ifdef USES_RTOS
+		//TODO We could use semaphores if RTOS is being used
+		vTaskDelay(10);
+#else
+		HAL_Delay(10);
+#endif
+	}
+	//Change sensor address and enable communication
+	HAL_GPIO_WritePin(__ToFX_SHUT_Port[this->__sensor_index], __ToFX_SHUT_Pin[this->__sensor_index], GPIO_PIN_SET);
+
+	//Set new address
+	ret = SetI2CAddress();
+	//Init sensor
+	ret = (vl53l5cx_init(&(this->__sensor_conf)) == 0)? HAL_OK : HAL_ERROR ;
+	//Increment __sensor_init_tbd
+	this->__sensor_init_tbd++;
 	return ret;
+}
+
+HAL_StatusTypeDef Sensor_vl53l5cx::SetI2CAddress()
+{
+    uint8_t ret = vl53l5cx_set_i2c_address(&this->__sensor_conf, __ToFAddr[this->__sensor_index]);
+	return (ret == 0)? HAL_OK : HAL_ERROR;
 }
 
